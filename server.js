52190a58aa3613f1feda498b1f048a4470af4c4b
Ahmedmhmud <ahmedmahmo431@gmail.com>
2026-08-29 15:39:45 +0300

    simple routing

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 import http from 'http';
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    //res.setHeader('Content-Type', 'text/html');
-
-    res.writeHead(500, { 'Content-Type': 'application/json' })
-
-    res.end(JSON.stringify({ message: 'Server error' }));
+    try {
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end('<h1>Homepage</h1>');
+            } else if (req.url === '/about') {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end('<h1>About</h1>');
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' })
+                res.end('<h1>Not Found</h1>');
+            }
+        } else {
+            throw new Error('Method is not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' })
+        res.end('Server Error');
+    }
 });
 
 server.listen(PORT, () => {
