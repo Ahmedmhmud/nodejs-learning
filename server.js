5da06d40b3b59e80581f352575dc342aaf78e861
Ahmedmhmud <ahmedmahmo431@gmail.com>
2026-08-29 15:51:01 +0300

    loading files

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 
 const PORT = process.env.PORT;
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = http.createServer((req, res) => {
     try {
         if (req.method === 'GET') {
+            let filepath;
             if (req.url === '/') {
-                res.writeHead(200, { 'Content-Type': 'text/html' })
-                res.end('<h1>Homepage</h1>');
+                filepath = path.join(__dirname, 'public','index.html');
             } else if (req.url === '/about') {
-                res.writeHead(200, { 'Content-Type': 'text/html' })
-                res.end('<h1>About</h1>');
+                filepath = path.join(__dirname, 'public','about.html');
             } else {
                 res.writeHead(404, { 'Content-Type': 'text/html' })
                 res.end('<h1>Not Found</h1>');
             }
+
+            const data = await fs.readFile(filepath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
         } else {
             throw new Error('Method is not allowed');
         }
