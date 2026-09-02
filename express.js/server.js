dec87a3268e7b6601b91af8cf8e643db2e2f319f
Ahmedmhmud <ahmedmahmo431@gmail.com>
2026-09-02 15:42:54 +0300

    sending files

--- express.js/server.js
+++ express.js/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, World!</h1>');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.send('<h1>about</h1>');
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(8000, () => {
