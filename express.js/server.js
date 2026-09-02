54a308daedb66408f287eac8e257d5ac23f9d702
Ahmedmhmud <ahmedmahmo431@gmail.com>
2026-09-02 21:36:37 +0300

    router module

--- express.js/server.js
+++ express.js/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require('path');
+const posts = require('./routes/posts');
 
 const PORT = process.env.PORT || 8000;
 
@@ -8,31 +9,7 @@ const app = express();
 // Setup static server
 // app.use(express.static(path.join(__dirname, "public")));
 
-let posts = [
-    { id: 1, title: "Open Sauce" },
-    { id: 2, title: "Open Source" },
-    { id: 3, title: "Open Course" }
-]
-
-app.get('/api/posts', (req, res) => {
-    const limit = parseInt(req.query.limit);
-    if (!isNaN(limit) && limit > 0) {
-        return res.json(posts.slice(0, limit));
-    }
-
-    res.json(posts);
-});
-
-app.get('/api/posts/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    if (!isNaN(id)) {
-        let post = posts.find((post) => post.id === id);
-        if (post) return res.status(200).json(post);
-        return res.status(404).json({ message: "Post not found" });
-    }
-
-    res.status(400).json({ message: "invalid id" });
-});
+app.use('/api/posts', posts);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
