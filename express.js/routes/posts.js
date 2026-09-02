4ea90bfff07591f15b10d36c1d641ca3cd4aff38
Ahmedmhmud <ahmedmahmo431@gmail.com>
2026-09-02 23:04:06 +0300

    deleting a specific post

--- express.js/routes/posts.js
+++ express.js/routes/posts.js
@@ -56,4 +56,19 @@ router.put('/:id', (req, res) => {
     res.status(200).json(post);
 });
 
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (isNaN(id) || id < 0) {
+        return res.status(400).json({ message: "ID must be a positive number" });
+    }
+
+    const post = posts.find((post) => post.id === id);
+    if (!post) {
+        return res.status(404).json({ message: "Post does not exist" });
+    }
+
+    posts = posts.filter((post) => post.id !== id);
+    res.status(200).json(posts);
+});
+
 export default router;
