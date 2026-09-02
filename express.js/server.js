8ea66513e81dcff9396be43a2983181d1ff00b7b
Ahmedmhmud <ahmedmahmo431@gmail.com>
2026-09-02 15:36:49 +0300

    Basic server & watch flag & npm scripts

---
+++ express.js/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, World!</h1>');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>about</h1>');
+});
+
+app.listen(8000, () => {
+    console.log(`Server running on port 8000`)
+});
